9e2a47f157ef385cf3dcc8abf079b4573b90de4e
kevin <k.ev.91@hotmail.fr>
2020-02-18 16:11:50 +0100

    javascript sur ajout_evenement

---
+++ scripts/select.js
@@ -0,0 +1,9 @@
+function THEFUNCTION(i) {
+		var divSuperficie = document.getElementById('divSuperficie');
+		switch(i) {
+			
+			case 1 : divSuperficie.style.display = ''; divMeteo.style.display = 'none'; break;
+			case 2 : divMeteo.style.display = ''; divSuperficie.style.display = 'none'; break;
+			default: divSuperficie.style.display = 'none'; divMeteo.style.display = 'none'; break;
+		}
+	}
--- vue/ajout_evenement.php
+++ vue/ajout_evenement.php
@@ -1,15 +1,35 @@
 
 
 					<center>
-					<h1> Ajouter un évènement intérieur </h1>
+					<h1> Ajouter un évènement </h1>
                     <form method ="post" action ="">
-                    <tr>Lieu de l'événement : <input type='text' name='lieu'> </tr>
-                    <tr>Nom de l'événement <input type='text' name='libelle'> </tr>
-					<tr>date de l'événement <input type='date' name='dateEV'> </tr>
+					<div>
+						Lieu de l'événement : <input type='text' name='lieu'> 
+					</div>
+					<div>
+						Nom de l'événement <input type='text' name='libelle'>
+					</div>
+					<div>
+						date de l'événement <input type='date' name='dateEV'>
+					</div>
+					<div>					
+						Type d'événement<SELECT id="leSelect" name="uneTable" size="1" onChange="THEFUNCTION(this.selectedIndex);">
+							<option value ='choix'>choix</option>
+							<option value ='interieur'>intérieur</option>
+							<option value ='exterieur'>extérieur</option>
+						</SELECT>		
+					</div>
+					<div style="display:none;" id="divSuperficie">
+						Superficie <input type='text' name='superficie'> 
+					</div>
 					
-					<tr>Superficie <input type='text' name='superficie'> </tr>
+					<div style="display:none;" id="divMeteo">
+						Meteo <input type='text' name='meteo'>
+					</div>
 					
                     <input type ="reset" name ="Annuler" value ="Annuler">
                     <input type ="submit" name ="ajouter" value ="ajouter"><br/>
                     </form>
 					</center>
+					
+					<script src="scripts/select.js"></script>
