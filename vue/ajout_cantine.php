d42791defe85c678e36ea36c965eab603582fe57
kevin <k.ev.91@hotmail.fr>
2020-05-05 22:08:30 +0200

    supprimer ajouter

--- vue/ajout_cantine.php
+++ vue/ajout_cantine.php
@@ -1,34 +1,58 @@
 		<h1> Ajouter une cantine </h1> </br>
                     <form method ="post" action ="">
 					
+					<table>
+					<tr>
+					<td><label for="ville">Ville : </label></td>				<td><input type='text' name='ville' id="ville"></td>
+					</tr>
+					
+					<tr>
+						<td><label for="codePostal"> Code Postal : </label></td>			<td><input type='text' name='codePostal' id="codePostal"></td>
+					</tr>
+					
+					<tr>
+						<td><label for="prix">Prix : </label></td>			<td><input type='text' name='prix' id="prix"></td>
+					</tr>
+					
+					<tr>
+						<td><label for="leSelect">Type de cantine : </label></td>			<td><SELECT id="leSelect" name="uneTable" size="1" onChange="THEFUNCTION1(this.selectedIndex);">
+																									<option value ='choix'>Choix : </option>
+																									<option value ='publique'>Publique</option>
+																									<option value ='privee'>Privée</option>
+																								</SELECT></td>
+					</tr>
+					
+					<tr>
+						<td></td>			<td></td>
+					</tr>
+					
+					
+					</table>
+					
 						
 					
 				
-						<label for="ville">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;Ville : </label><input type='text' name='ville' id="ville"> </br>
+						
 						
 					
-						<label for="codePostal"> Code Postal : </label><input type='text' name='codePostal' id="codePostal"> </br>
+						
 					
 										
 						
-						<label for="prix">&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;Prix : </label><input type='text' name='prix' id="prix"> </br>
+						
 
-						<label for="leSelect">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Type de cantine : </label>
-						<SELECT id="leSelect" name="uneTable" size="1" onChange="THEFUNCTION1(this.selectedIndex);">
-							<option value ='choix'>Choix : </option>
-							<option value ='publique'>Publique</option>
-							<option value ='privee'>Privée</option>
-						</SELECT>		</br>
+						
+								
 					
 					
 					
 					<div style="display:none;" id="divReduction">
-						<label for="reduction"> Réduction </label>
+						<label for="reduction"> Réduction : </label>
 						<input type='text' name='reduction' id="reduction">
 					</div>
 
 					<div style="display:none;" id="divBourse">
-						<label for="bourse"> Bourse </label>
+						<label for="bourse"> Bourse : </label>
 						<input type='text' name='bourse' id="bourse">
 					</div>
 					
--- vue/supprimer_cantine.php
+++ vue/supprimer_cantine.php
@@ -1,18 +1,34 @@
 <h1> Supprimer une cantine </h1>
 					<form method ="post" action ="">
-					Cantine à supprimer : 
-					<td><SELECT name="idC" size="1">
-					<?php
-					$connexion = mysqli_connect("localhost","root","","mairie");
-					$requete="select * from cantine;";
-					$resultats=mysqli_query($connexion,$requete);
-					while ($ligne = mysqli_fetch_assoc($resultats))
-							{
-								echo"<option value ='".$ligne["idC"]."'>".$ligne["ville"]."</option>";
-							}	 
+					<table>
+					
+					<tr>
+						<td>
+						Cantine à supprimer : 
+						</td>
+						<td>
+							<SELECT name="idC" size="1">
+							<?php
+							$connexion = mysqli_connect("localhost","root","","mairie");
+							$requete="select * from cantine;";
+							$resultats=mysqli_query($connexion,$requete);
+							while ($ligne = mysqli_fetch_assoc($resultats))
+									{
+										echo"<option value ='".$ligne["idC"]."'>".$ligne["ville"]."</option>";
+									}	 
 
-						 ?>
-						 </SELECT></td>
+								 ?>
+								 </SELECT>
+						</td>
+					</tr>
+					
+					<tr>
+						<td>
 					<input type ="reset" name ="Annuler" value ="Annuler">
 					<input type ="submit" name ="supprimer" value ="supprimer"><br/>
+						</td>
+					</tr>
+					
+					</table>
+					
 					</form>
